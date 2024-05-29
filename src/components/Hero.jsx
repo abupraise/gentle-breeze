6e583eb035df49488c8b186a4d4a9aae5defc0b5
Captain_Sani <sani.jdogo@gmail.com>
2024-05-29 17:54:36 +0100

    commit files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div>
+        <div>
+            <h1>ELEVATE YOUR <br>WORKOUT</br></h1>
+
+            <p>Lorem ipsum</p>
+        </div>
+
+
+    </div>
+  )
+}
