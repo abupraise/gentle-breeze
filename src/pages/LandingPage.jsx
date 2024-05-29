f3337782c2f1b483abeb02e098708afce2601720
Captain_Sani <sani.jdogo@gmail.com>
2024-05-29 17:54:36 +0100

    commit files

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Payment from '../components/Payment'
 import Reviews from '../components/Reviews'
+import { Navbar } from '../components/Navbar'
+import { Hero } from '../components/Hero'
 
 const LandingPage = () => {
   return (
     <>
+    <Navbar />
+    <Hero />
       <Payment/>
       <Reviews/>
     </>
