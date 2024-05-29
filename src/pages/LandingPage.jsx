0a9db6947d9ee1c08a11ddc58023f84af39eb440
Captain_Sani <sani.jdogo@gmail.com>
2024-05-29 17:54:36 +0100

    commit files

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,20 @@
+
 import React from "react";
 import Payment from "../components/Payment";
 import Reviews from "../components/Reviews";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
 
+
 const LandingPage = () => {
   return (
     <>
+
       <Payment />
       <Reviews />
       <Contact />
       <Footer />
+
     </>
   );
 };
